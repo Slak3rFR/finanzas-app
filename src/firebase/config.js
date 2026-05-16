77004c77e3cb56843d2c8b9e2b59fc0814b92d6a
Franco Reynoso <francobv89@gmail.com>
2026-05-16 02:20:05 -0300

    Movidas keys Firebase a variables de entorno

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,20 +1,48 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
-import { getFirestore } from 'firebase/firestore'
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {
+  getFirestore,
+} from 'firebase/firestore'
+
+import {
+  getAuth,
+} from 'firebase/auth'
+
 const firebaseConfig = {
-    authDomain: "finanzas-app-c9d40.firebaseapp.com",
-    apiKey: "AIzaSyBoUzmnbTgn4hSvjE1b7mfXyfG8caLcHaE",
-    storageBucket: "finanzas-app-c9d40.firebasestorage.app",
-    projectId: "finanzas-app-c9d40",
-    appId: "1:424187622399:web:054817ca5b73660c5dedca",
-    messagingSenderId: "424187622399",
-    measurementId: "G-EN26JCWV9W"
-};
 
-const app = initializeApp(firebaseConfig)
+  apiKey:
+    import.meta.env
+      .VITE_FIREBASE_API_KEY,
 
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+  authDomain:
+    import.meta.env
+      .VITE_FIREBASE_AUTH_DOMAIN,
+
+  projectId:
+    import.meta.env
+      .VITE_FIREBASE_PROJECT_ID,
+
+  storageBucket:
+    import.meta.env
+      .VITE_FIREBASE_STORAGE_BUCKET,
+
+  messagingSenderId:
+    import.meta.env
+      .VITE_FIREBASE_MESSAGING_SENDER_ID,
+
+  appId:
+    import.meta.env
+      .VITE_FIREBASE_APP_ID,
+
+}
+
+const app =
+  initializeApp(
+    firebaseConfig
+  )
+
+export const db =
+  getFirestore(app)
+
+export const auth =
+  getAuth(app)
